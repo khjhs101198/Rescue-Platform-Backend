929335c5fb7618c34adb602d2df13d386d197ed8
TsenHill <chou870808@gmail.com>
2022-02-12 22:56:55 +0800

    Create a volunteer-related table.

---
+++ SQL_data_preprocess/SQL_Table/MariaDB_TaskPackage_Create.py
@@ -0,0 +1,47 @@
+import mysql.connector
+from mysql.connector import Error
+try:
+    # 連接 MySQL/MariaDB 資料庫
+    connection = mysql.connector.connect(
+        host='localhost',          # 主機名稱
+        database='smartdb', # 資料庫名稱
+        user='Tsen',        # 帳號
+        password='CTsen')  # 密碼
+
+    if connection.is_connected():
+
+        # 顯示資料庫版本
+        db_Info = connection.get_server_info()
+        print("資料庫版本：", db_Info)
+
+        # 顯示目前使用的資料庫
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("目前使用的資料庫：", record)
+
+    cursor = connection.cursor()
+
+    #Creating table as per requirement
+    sql ='''CREATE TABLE task_package(
+    id INT NOT NULL AUTO_INCREMENT,
+    latitude DECIMAL(10,7) NOT NULL,
+    longitude DECIMAL(10,7) NOT NULL,
+    task_date DATE;
+    taskinfo VARCHAR(500),
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES volunteers(id)
+    );'''
+    cursor.execute(sql)
+
+    #Closing the connection
+    connection.close()
+
+except Error as e:
+    print("資料庫連接失敗：", e)
+
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("資料庫連線已關閉")
---
+++ SQL_data_preprocess/SQL_Table/MariaDB_Volunteers_Create.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import Error
+try:
+    # 連接 MySQL/MariaDB 資料庫
+    connection = mysql.connector.connect(
+        host='localhost',          # 主機名稱
+        database='smartdb', # 資料庫名稱
+        user='Tsen',        # 帳號
+        password='CTsen')  # 密碼
+
+    if connection.is_connected():
+
+        # 顯示資料庫版本
+        db_Info = connection.get_server_info()
+        print("資料庫版本：", db_Info)
+
+        # 顯示目前使用的資料庫
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("目前使用的資料庫：", record)
+
+    cursor = connection.cursor()
+
+    #Creating table as per requirement
+    sql ='''CREATE TABLE volunteers(
+        id INT NOT NULL AUTO_INCREMENT,
+        have_task TINYINT;
+        state TINYINT;
+        latitude DECIMAL(10,7) NOT NULL,
+        longitude DECIMAL(10,7) NOT NULL,
+        PRIMARY KEY(id)
+        );'''
+    cursor.execute(sql)
+
+    #Closing the connection
+    connection.close()
+
+except Error as e:
+    print("資料庫連接失敗：", e)
+
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("資料庫連線已關閉")
